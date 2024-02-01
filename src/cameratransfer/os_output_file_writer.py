193bf56384a88ecb61b4412cbdb74f637032444d
Chris Denney <chris.j.denney@gmail.com>
2024-02-01 21:51:21 +0000

    ensure output file writer has the correct YYYY / MM / DD folder names

--- src/cameratransfer/os_output_file_writer.py
+++ src/cameratransfer/os_output_file_writer.py
@@ -1,17 +1,20 @@
 import logging
 from datetime import datetime
 from pathlib import Path
+from dataclasses import dataclass
 
 logger = logging.getLogger(__name__)
 
+@dataclass
 class OSOutputFileWriter:
-    def __init__(self, location: Path):
-        self.location: Path = location
+    base_location: Path
+    sub_location: Path = Path(datetime.now().strftime("%Y/%m/%d"))
 
     def write_file(self, file_name: str, file_last_modified: datetime, file_content: bytes) -> None:
         logger.debug(f"file_name: {file_name}")
         logger.debug(f"file_last_modified: {file_last_modified}")
-        self.location.joinpath(file_name).touch(exist_ok=False)
-
-        with open(self.location.joinpath(file_name), "wb") as f:
-            f.write(file_content)
+        fq_file_name = self.base_location.joinpath(self.sub_location, file_name)
+        
+        fq_file_name.parent.mkdir(parents=True, exist_ok=True)
+        fq_file_name.touch(exist_ok=False)
+        fq_file_name.write_bytes(file_content)
--- tests/test_camera_transfer.py
+++ tests/test_camera_transfer.py
@@ -6,6 +6,7 @@ from cameratransfer.os_file_getter import OSFileGetter
 from cameratransfer.os_output_file_writer import OSOutputFileWriter
 from cameratransfer.hash_store import HashStore
 from cameratransfer.camera_file_getter import CameraFileGetter
+from datetime import datetime
 
 import logging
 logger = logging.getLogger(__name__)
@@ -57,57 +58,58 @@ def duplicate_video_camera_file_getter(duplicate_video_os_file_getter):
 
 @pytest.fixture
 def temp_os_output_file_writer(tmp_path):
-    return OSOutputFileWriter(location=tmp_path)
+    return OSOutputFileWriter(base_location=tmp_path)
 
-def test_camera_transfer(single_image_camera_file_getter, temp_os_output_file_writer):
+def test_camera_transfer(single_image_camera_file_getter, temp_os_output_file_writer, tmp_path):
     camera_transfer_operation = CameraTransfer(
         camera_file_getter=single_image_camera_file_getter,
         output_file_writer=temp_os_output_file_writer,
         hash_store=HashStore(filename=":memory:"),
     )
     camera_transfer_operation.run()
-    assert len(list(temp_os_output_file_writer.location.iterdir())) == 1
-    expected_output_file = temp_os_output_file_writer.location.joinpath("2022-07-27T115409_S9700_6228.JPG")
+    assert len(list(temp_os_output_file_writer.base_location.iterdir())) == 1
+
+    expected_output_file = Path(tmp_path) / datetime.now().strftime("%Y") / datetime.now().strftime("%m") / datetime.now().strftime("%d") / "2022-07-27T115409_S9700_6228.JPG"
     assert expected_output_file.exists()
     assert expected_output_file.stat().st_size == 3560217
     
 
-def test_camera_transfer_duplicate(duplicate_image_camera_file_getter, temp_os_output_file_writer):
+def test_camera_transfer_duplicate(duplicate_image_camera_file_getter, temp_os_output_file_writer, tmp_path):
     camera_transfer_operation = CameraTransfer(
         camera_file_getter=duplicate_image_camera_file_getter,
         output_file_writer=temp_os_output_file_writer,
         hash_store=HashStore(filename=":memory:")
     )
     camera_transfer_operation.run()
-    assert len(list(temp_os_output_file_writer.location.iterdir())) == 1
+    assert len(list(temp_os_output_file_writer.base_location.iterdir())) == 1
 
-    expected_output_file = temp_os_output_file_writer.location.joinpath("2022-07-27T115409_S9700_6228.JPG")
+    expected_output_file = Path(tmp_path) / datetime.now().strftime("%Y") / datetime.now().strftime("%m") / datetime.now().strftime("%d") / "2022-07-27T115409_S9700_6228.JPG"
     assert expected_output_file.exists()
     assert expected_output_file.stat().st_size == 3560217
 
 
-def test_video_transfer(single_video_camera_file_getter, temp_os_output_file_writer):
+def test_video_transfer(single_video_camera_file_getter, temp_os_output_file_writer, tmp_path):
     camera_transfer_operation = CameraTransfer(
         camera_file_getter=single_video_camera_file_getter,
         output_file_writer=temp_os_output_file_writer,
         hash_store=HashStore(filename=":memory:"),
     )
     camera_transfer_operation.run()
-    assert len(list(temp_os_output_file_writer.location.iterdir())) == 1
+    assert len(list(temp_os_output_file_writer.base_location.iterdir())) == 1
 
-    expected_output_file = temp_os_output_file_writer.location.joinpath("2024-01-25T170003_video.mp4")
+    expected_output_file = Path(tmp_path) / datetime.now().strftime("%Y") / datetime.now().strftime("%m") / datetime.now().strftime("%d") / "2024-01-25T170003_video.mp4"
     assert expected_output_file.exists()
     assert expected_output_file.stat().st_size == 1311047
 
-def test_video_transfer_duplicate(duplicate_video_camera_file_getter, temp_os_output_file_writer):
+def test_video_transfer_duplicate(duplicate_video_camera_file_getter, temp_os_output_file_writer, tmp_path):
     camera_transfer_operation = CameraTransfer(
         camera_file_getter=duplicate_video_camera_file_getter,
         output_file_writer=temp_os_output_file_writer,
         hash_store=HashStore(filename=":memory:"),
     )
     camera_transfer_operation.run()
-    assert len(list(temp_os_output_file_writer.location.iterdir())) == 1
+    assert len(list(temp_os_output_file_writer.base_location.iterdir())) == 1
 
-    expected_output_file = temp_os_output_file_writer.location.joinpath("2024-01-25T170003_video.mp4")
+    expected_output_file = Path(tmp_path) / datetime.now().strftime("%Y") / datetime.now().strftime("%m") / datetime.now().strftime("%d") / "2024-01-25T170003_video.mp4"
     assert expected_output_file.exists()
     assert expected_output_file.stat().st_size == 1311047
