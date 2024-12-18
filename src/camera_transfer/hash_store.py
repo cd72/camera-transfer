57b656af883ebb25d49fd17464c0734a39b2341b
Chris Denney <chris.j.denney@gmail.com>
2024-12-18 22:58:33 +0000

    Refactor HashStore initialization and improve database connection handling

--- src/camera_transfer/hash_store.py
+++ src/camera_transfer/hash_store.py
@@ -1,30 +1,26 @@
 import sqlite3
 from pathlib import Path
+from typing import Optional
 
 
-class HashStore():
-    def __init__(self, database_file: Path | None, dry_run: bool = False) -> None:
-        if database_file is None:
-            database_file_name = ""
-        else:
+class HashStore:
+    def __init__(self, database_file: Optional[Path] = None, dry_run: bool = False) -> None:
+        database_file_name = str(database_file) if database_file else ":memory:"
+        if database_file:
             database_file.parent.mkdir(parents=True, exist_ok=True)
-            database_file_name = str(database_file)
 
         self.conn = sqlite3.connect(database_file_name)
         self.conn.execute(
-            "CREATE TABLE IF NOT EXISTS hash_store (hash text unique, image_file text)"
+            "CREATE TABLE IF NOT EXISTS hash_store (hash TEXT UNIQUE, image_file TEXT)"
         )
         self.dry_run = dry_run
 
     def __setitem__(self, hash: bytes, file_item: str) -> None:
         if not self.dry_run:
-            self.conn.execute("INSERT OR REPLACE INTO hash_store VALUES (?, ?)", (hash, file_item))
-            self.conn.commit()
+            with self.conn:
+                self.conn.execute("INSERT OR REPLACE INTO hash_store VALUES (?, ?)", (hash, file_item))
 
-    
-    def __contains__(self, hash:bytes) -> bool:
+    def __contains__(self, hash: bytes) -> bool:
         cursor = self.conn.execute("SELECT image_file FROM hash_store WHERE hash=?", (hash,))
-        row = cursor.fetchone()
-        return row is not None
-
+        return cursor.fetchone() is not None
     
